74b5632ad6d5b3a3434b9451dc593083a4b1d7ff
P.T Nyamuda <ptnrlab@gmail.com>
2025-08-09 06:39:29 +0200

    created topic store actions

---
+++ src/stores/topic.ts
@@ -0,0 +1,109 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import axios from "axios";
+import type { Topic } from "@/models/topic";
+import { UrlHelper } from "@/helpers/urlHelper";
+import { ErrorResponse } from "@/models/errorResponse";
+import type { PageInfo } from "@/models/pageInfo";
+
+export const useTopicStore = defineStore("topic", () => {
+  const apiUrl = ref(`${UrlHelper.apiUrl}/topics`);
+
+  //Gets a topic with a given ID
+  const getTopicById = (id: number): Promise<Topic> => {
+    return new Promise((resolve, reject) => {
+      axios
+        .get<Topic>(`${apiUrl.value}/${id}`)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((err) => {
+          const message =
+            err.response.statusCode == 404
+              ? ErrorResponse.NotFound("Topic")
+              : ErrorResponse.Unexpected();
+
+          reject(message);
+        });
+    });
+  };
+
+  //Gets a paginated list of topics along with pagination metadata
+  const getTopics = (page: number, pageSize: number): Promise<PageInfo<Topic>> => {
+    return new Promise((resolve, reject) => {
+      axios
+        .get<PageInfo<Topic>>(`${apiUrl.value}`, {
+          params: {
+            page: page,
+            pageSize: pageSize,
+          },
+        })
+        .then((response) => {
+          //return the topics
+          resolve(response.data);
+        })
+        .catch((err) => {
+          const message = err.response.data?.message || ErrorResponse.Unexpected();
+          reject(message);
+        });
+    });
+  };
+
+  //Creates a new topic
+  const addTopic = (details: { name: string; subjectIds: number[] }) => {
+    return new Promise((resolve, reject) => {
+      //add access token to the request
+      //to access the protected route
+      setAuthToken();
+      axios
+        .post(`${apiUrl.value}`, details)
+        .then(() => resolve({}))
+        .catch((err) => {
+          const message = err.response.data?.message || ErrorResponse.Unexpected();
+          reject(message);
+        });
+    });
+  };
+
+  //Updates a topic with a given ID
+  const updateTopic = (id: number, updateDetails: { name: string; subjectIds: number[] }) => {
+    return new Promise((resolve, reject) => {
+      //add access token to the request
+      //to access the protected route
+      setAuthToken();
+      axios
+        .put(`${apiUrl.value}/${id}`, updateDetails)
+        .then(() => resolve({}))
+        .catch((err) => {
+          const message = err.response.data?.message || ErrorResponse.Unexpected();
+          reject(message);
+        });
+    });
+  };
+
+  //Deletes a topic with a given ID
+  const deleteTopic = (id: number) => {
+    return new Promise((resolve, reject) => {
+      //add access token to the request
+      //to access the protected route
+      setAuthToken();
+      axios
+        .delete(`${apiUrl.value}/${id}`)
+        .then(() => resolve({}))
+        .catch((err) => {
+          const message = err.response.data?.message || ErrorResponse.Unexpected();
+          reject(message);
+        });
+    });
+  };
+
+  //Set authorization header for all requests to access protected routes from the API
+  const setAuthToken = () => {
+    //get the access token from local storage
+    const token = localStorage.getItem("jwt_token");
+
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  };
+
+  return { getTopicById, getTopics, updateTopic, deleteTopic, addTopic };
+});
