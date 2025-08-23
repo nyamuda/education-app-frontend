07fa1cb8840c2fcc122ea8caef4c06d9b1b72f6f
P.T Nyamuda <ptnrlab@gmail.com>
2025-08-23 07:56:56 +0200

    created the `SubjectQueryParams` interface

---
+++ src/interfaces/topics/topicQueryParams.ts
@@ -0,0 +1,11 @@
+import type { TopicSortOption } from "@/enums/topics/topicSortOption";
+
+export interface SubjectQueryParams {
+  page: number;
+  pageSize: number;
+  sortBy: TopicSortOption;
+  curriculumId: number | null;
+  examBoardId: number | null;
+  levelId: number | null;
+  subjectId: number | null;
+}
